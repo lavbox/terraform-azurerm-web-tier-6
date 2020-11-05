cc98791a14fee5155f43a32a83f55d9d3997d4f8
Harun Davood <Harun.Davood@exeloncorp.com>
2020-11-05 16:35:00 -0500

    module publish

---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "client_secret" {
+  type = string
+}
+
+variable "regionPrefix" {
+  type = string
+}
+
+variable "tierPrefix" {
+  type = string
+}
+
+variable "opcos" {
+  type = list(string)
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "project_short_name" {
+  type = string
+}
