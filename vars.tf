9adc3811b4942bc4faf2a6d744e9611fe8f32641
Harun Davood <Harun.Davood@exeloncorp.com>
2020-11-05 18:05:01 -0500

    removes unecessary vars

--- vars.tf
+++ vars.tf
@@ -13,3 +13,15 @@ variable "project_name" {
 variable "project_short_name" {
   type = string
 }
+
+variable "opcos" {
+  type = set(string)
+  default =  [
+    "BGE",
+    "COMED",
+    "PECO",
+    "DPL",
+    "ACE",
+    "PEPCO"
+  ]
+}
